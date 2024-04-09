8c8889971f3287145780ff4e4fb61652e1015991
Anubha1998 <121358222+Anubha1998@users.noreply.github.com>
2024-04-09 18:07:38 +0530

    Update TestNGTodo1.java

--- TestNGTodo1.java
+++ TestNGTodo1.java
@@ -64,43 +64,8 @@ public class TestNGTodo1 {
 
         Thread.sleep (1000);
 
-
-      //  driver.findElement(By.id("sampletodotext")).sendKeys(" List Item 6");
-      //  driver.findElement(By.id("addbutton")).click();
-
-       // driver.findElement(By.id("sampletodotext")).sendKeys(" List Item 7");
-       // driver.findElement(By.id("addbutton")).click();
-
-       // driver.findElement(By.id("sampletodotext")).sendKeys(" List Item 8");
-      //  driver.findElement(By.id("addbutton")).click();
-
-      //  System.out.println("Checking Another Box");
-      //  driver.findElement(By.name("li1")).click();
-
-      //  System.out.println("Checking Another Box");
-       // driver.findElement(By.name("li3")).click();
-
-       // System.out.println("Checking Another Box");
-      //  driver.findElement(By.name("li7")).click();
-
-      //  System.out.println("Checking Another Box");
-       // driver.findElement(By.name("li8")).click();
-      //  Thread.sleep(300);
-
-       // System.out.println("Entering Text");
-      //  driver.findElement(By.id("sampletodotext")).sendKeys("Get Taste of Lambda and Stick to It");
-
-       // driver.findElement(By.id("addbutton")).click();
-
-      //  System.out.println("Checking Another Box");
-      //  driver.findElement(By.name("li9")).click();
-
-        // Let's also assert that the todo we added is present in the list.
-
-      //  spanText = driver.findElementByXPath("/html/body/div/div/div/ul/li[9]/span").getText();
-      //  Assert.assertEquals("Get Taste of Lambda and Stick to It", spanText);
-      //  Status = "passed";
-     //   Thread.sleep(150);
+        Status = "passed";
+        Thread.sleep(150);
 
         System.out.println("TestFinished");
         driver.quit();
